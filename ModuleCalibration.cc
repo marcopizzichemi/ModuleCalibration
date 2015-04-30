d78b39bcfe9fd65876cdf5895f91a71cd565193a
Marco Pizzichemi <marco.pizzichemi@gmail.com>
2015-04-30 20:54:24 +0200

    Empty project

---
+++ ModuleCalibration.cc
@@ -0,0 +1,56 @@
+//------------------------------------------------------------//
+//                                                            //
+//  PROGRAM FOR matrix calibration USING DT5740               //
+//                                                            //
+//------------------------------------------------------------//
+
+// compile with 
+// g++ -o ModuleCalibration ModuleCalibration.cpp `root-config --cflags --glibs` -lSpectrum -lMLP -lTreePlayer
+
+#include "TROOT.h"
+#include "TStyle.h"
+#include "TSystem.h"
+#include "TLegend.h"
+#include "TCanvas.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TString.h"
+#include "TApplication.h"
+#include "TLegend.h"
+#include "TTree.h"
+#include "TFile.h"
+#include "TF2.h"
+#include "TSpectrum.h"
+#include "TSpectrum2.h"
+#include "TTreeFormula.h"
+#include "TMath.h"
+#include "TChain.h"
+#include "TCut.h"
+#include "TLine.h"
+#include "TError.h"
+#include "TEllipse.h"
+#include "TFormula.h"
+#include "TGraphErrors.h"
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+#include <stdlib.h> 
+#include <stdio.h> 
+#include <unistd.h>
+#include <cmath> 
+
+#include "ConfigFile.h"
+
+
+
+int main (int argc, char** argv)
+{
+  
+  return 0;
+}
---
+++ build/CMakeFiles/ModuleCalibration.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/marco/Universita/Cern/NewClearPEM/AnalysisSoftware/pet_programs/ModuleCalibration/ModuleCalibration.cc" "/home/marco/Universita/Cern/NewClearPEM/AnalysisSoftware/pet_programs/ModuleCalibration/build/CMakeFiles/ModuleCalibration.dir/ModuleCalibration.cc.o"
+  "/home/marco/Universita/Cern/NewClearPEM/AnalysisSoftware/pet_programs/ModuleCalibration/src/ConfigFile.cc" "/home/marco/Universita/Cern/NewClearPEM/AnalysisSoftware/pet_programs/ModuleCalibration/build/CMakeFiles/ModuleCalibration.dir/src/ConfigFile.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/marco/Programs/rootsource/include"
+  "../include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
