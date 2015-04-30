1ea0d832ea84e3d77aa628f08d801a4591f11672
Marco Pizzichemi <marco.pizzichemi@gmail.com>
2015-04-30 22:21:24 +0200

    Starting to play with config file

--- ModuleCalibration.cc
+++ ModuleCalibration.cc
@@ -52,5 +52,61 @@
 int main (int argc, char** argv)
 {
   
+  //----------------------------------------------------------//
+  //  Check input                                             //
+  //----------------------------------------------------------//
+  if(argc<2) 
+  {
+    std::cout << " Usage: " 							<< std::endl;
+    std::cout << " ModuleCalibration [-c config file ] <input-files> " 		<< std::endl;
+    std::cout << "   note: -c option is optional" 				<< std::endl;
+    std::cout << "         without it, a configFile.txt will be assumed" 	<< std::endl;
+    return 0;
+  }
+  
+  //----------------------------------------------------------//
+  //  Say hello to the user                                   //
+  //----------------------------------------------------------//
+  std::cout<<"\n"<<std::endl;
+  std::cout<<"###########################################################"<<std::endl;  
+  std::cout<<"#                                                          #"<<std::endl;
+  std::cout<<"#           New Clear PEM module calibration               #"<<std::endl;  
+  std::cout<<"#                                                          #"<<std::endl;  
+  std::cout<<"###########################################################"<<std::endl;
+  std::cout<<"\n\n"<<std::endl;
+  std::cout<<"=====>   C O N F I G U R A T I O N   <====\n"<<std::endl;
+   
+  //----------------------------------------------------------//
+  //  Parse the config file                                   //
+  //----------------------------------------------------------//
+  // Set a default config file name
+  std::string ConfigFileName = "config.cfg"; 
+  // and assume there is no config input from command line 
+  bool configIsFromCommandLine = false;
+  // if passed by command line, override the config file name
+  for (int i = 0; i < argc ; i++)
+  {
+    if(std::string(argv[i]) == std::string("-c")) 
+    {
+      ConfigFileName = argv[i+1];
+      std::cout << "Configuration file: '" << argv[i+1] << "'"<< std::endl;
+      configIsFromCommandLine = true;
+    }
+  }
+  if(!configIsFromCommandLine)
+  {
+    std::cout << "Configuration file set to default: config.cfg "<< std::endl;
+  }
+  //open the config file
+  ConfigFile config(ConfigFileName);
+  
+  //read crystal dimensions
+  std::string dummy = config.read<std::string>("dummy");
+  int d1 = config.read<int>("ddd");
+  int d2 = config.read<int>("ddd");
+  std::cout << "Dummy: " << dummy << " " << d1 << " " << d2 << " "  << std::endl;
+  
+  
+  
   return 0;
 }
